14fde06cdcbaf6d5c04ff445adf1cd437587906a
Ava <avatan1109@gmail.com>
2022-12-07 11:40:02 -0500

    add two endpoints

---
+++ CreateUser/__init__.py
@@ -0,0 +1,42 @@
+import azure.functions as func
+import logging
+import uuid
+
+
+def main(req: func.HttpRequest, doc: func.Out[func.Document]) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+
+    email = req.params.get('email')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if not email:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            email = req_body.get('email')
+
+    if name and email:
+        newdocs = func.DocumentList()
+        newproduct_dict = {
+            "id": str(uuid.uuid4()),
+            "name": name,
+            "email": email
+        }
+        newdocs.append(func.Document.from_dict(newproduct_dict))
+        doc.set(newdocs)
+        return func.HttpResponse(f"User, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200
+        )
