394ee236bb78f13770fefd61031dd28f4f0d8f98
Angela <ac2677@cornell.edu>
2022-12-07 22:32:35 -0500

    added flask routing and deployed smartsnap:v2

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__, template_folder = "templates")
 def login():
   return render_template("index.html")
 
+@app.route('/home')
+def home():
+  return render_template("home.html")
+
+@app.route('/form')
+def form():
+  return render_template("form.html")
+
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=80)
